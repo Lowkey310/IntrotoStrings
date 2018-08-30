a94d4f24a4bacb24ea6aa1ef55ec8052d942b8eb
tianyu.zhou <tianyu.zhou@mu-c1-17.mullen.pvt.k12.co.us>
2018-08-30 08:50:23 -0600

    1Strings

---
+++ Intro to Strings/src/IntrotoStrings.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class IntrotoStrings
+	{
+
+		public static void main(String[] args)
+			{
+			   challenge1();
+
+			}
+
+		private static void challenge1()
+			{
+				System.out.println("JUST SHOUT!!!");
+				Scanner userInput = new Scanner(System.in);
+				String Ucase = userInput.nextLine();
+			
+				
+			}
+
+	}
