eec5f83c1cd64fc5b42199b2bcc758b4897143b6
tianyu.zhou <tianyu.zhou@mu-c1-17.mullen.pvt.k12.co.us>
2018-08-30 15:05:13 -0600

    intro2

--- Intro to Strings/src/IntrotoStrings.java
+++ Intro to Strings/src/IntrotoStrings.java
@@ -6,6 +6,7 @@ public class IntrotoStrings
 		public static void main(String[] args)
 			{
 			   challenge1();
+			   challenge2();
 
 			}
 
@@ -14,7 +15,23 @@ public class IntrotoStrings
 				System.out.println("JUST SHOUT!!!");
 				Scanner userInput = new Scanner(System.in);
 				String Ucase = userInput.nextLine();
-			
+				
+		        String Ucase1 = Ucase.toLowerCase();
+				System.out.println(Ucase.substring(0,1) + Ucase1.substring(1));
+			}
+		private static void challenge2()
+			{
+				System.out.println("JUST SHOUT!!!");
+				Scanner userInput = new Scanner(System.in);
+				String Ucase = userInput.nextLine();
+				
+			    String Ucase1 = Ucase.toLowerCase();
+				int Ucase2 = Ucase.length();
+				System.out.println(Ucase.substring(0,1));
+				for(int i=1; i<=Ucase2; i++)
+					{
+						System.out.println(Ucase1.substring(i,i+1));
+					}
 				
 			}
 
